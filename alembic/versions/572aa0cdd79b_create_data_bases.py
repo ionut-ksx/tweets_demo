29e3247977d11d7f5f241da2e6e7a6f2c3cbdcea
bws <bws@192-168-0-209.rdsnet.ro>
2022-07-04 22:26:51 +0300

    added model validations

---
+++ alembic/versions/572aa0cdd79b_create_data_bases.py
@@ -0,0 +1,24 @@
+"""create data bases
+
+Revision ID: 572aa0cdd79b
+Revises: 
+Create Date: 2022-07-04 20:28:35.833933
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '572aa0cdd79b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- src/tweets_demo/models/tweet.py
+++ src/tweets_demo/models/tweet.py
@@ -2,15 +2,36 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Index
 from sqlalchemy.orm import relationship, validates
 from tweets_demo.app import db
 from tweets_demo.models.user import User
+import re
 
 
 class Tweet(db.Model):
     __tablename__ = "tweets"
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
-    id_user = Column(Integer, ForeignKey=("user.id"), nullable=False)
-    id_comment = Column(Integer, ForeignKey=("content.id"), nullable=False)
+    id_user = Column(Integer, ForeignKey=("user.id"), Index=True, nullable=False)
     created_at = Column(String(255), nullable=False)
     content = Column(String(256), nullable=False)
 
+    def __init__(self, id_user, created_at, content):
+        self.id_user = self._is_valid_id_user(str(id_user))
+        self.created_at = self._is_valid_date_time(created_at)
+        self.content = self._is_valid_content(content)
+
     def __repr__(self):
         return f"id:{self.id}, id_user: {self.id_user}, created_at: {self.created_at}, content:{self.content}"
+
+    def _is_valid_id_user(self, id_user):
+        regex = "^[0-9][0-9]*$"
+        if not re.match(regex, id_user):
+            raise ValueError("User id is not correct")
+        return id_user
+
+    def _is_valid_date_time(self, created_at):
+        if not datetime.datetime.strptime(created_at, '%d-%m-%Y')
+            raise ValueError("Not a valid datetime")
+        return created_at
+
+    def _is_valid_content(self, content):
+        if not len(content) <= 256:
+            raise ValueError("Content cannot excede 256 characters.")
+        return content
